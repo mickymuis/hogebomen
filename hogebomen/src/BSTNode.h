4c41fcfbd20988e38bb7ee0f343599a815a04849
Micky Faas <micky@edukitty.org>
2014-11-25 12:06:11 +0100

    rotateLeft() and rotateRight() added to BSTNode

--- hogebomen/src/BSTNode.h
+++ hogebomen/src/BSTNode.h
@@ -37,6 +37,44 @@ template <class INFO_T> class BSTNode : public TreeNode<INFO_T>
         **/
       	BSTNode( BSTNode<INFO_T>* parent =0 )
             : S( (S)parent ) { }
+        
+        // Idea: rotate this node left and return the node that comes in its place
+        BSTNode *rotateLeft( ) {
+        
+            if( !rightChild( ) ) // Cannot rotate
+                return this;
+                
+            bool isLeftChild =parent( ) && this == parent( )->leftChild( );
+                
+            BSTNode *newTop =rightChild( );
+            BSTNode *newRight =newTop->leftChild( );
+            BSTNode *topParent =parent( );
+            
+            // We become left-child of our right-child
+            // newTop takes our place with our parent
+            newTop->setParent( topParent );
+            if( isLeftChild )
+                topParent->setLeftChild( newTop );
+            else
+                topParent->setRightChild( newTop );
+                
+            newTop->setLeftChild( this );
+            setParent( newTop );
+            
+            // We take the left-child of newTop as our right-child         
+            setRightChild( newRight );
+            if( newRight )
+                newRight->setParent( this );
+                
+            // We're done?
+            
+            return newTop;
+        }
+        
+        // Idea: rotate this node right and return the node that comes in its place
+        BSTNode *rotateRight( ) {
+        
+        }
 
         bool operator <( const BSTNode<INFO_T> &rhs ) {
             return S::info() < rhs.info();
